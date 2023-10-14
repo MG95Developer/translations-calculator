2bfc2aa78b67b921ae2f1bb537aba30e0f064229
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-10-14 12:51:49 +0100

    add final logic

--- src/main.js
+++ src/main.js
@@ -14,4 +14,42 @@ const resetPage = () =>
 	location.reload();
 }
 
-const resetBtn = document.getElementById('reset-btn').addEventListener("click", resetPage)
+const resetBtn = document
+	.getElementById('reset-btn')
+	.addEventListener('click', resetPage);
+
+// count words & calculate cost
+const pricePerWordInEuros = 0.1;
+
+const countWords = () => {
+	let words = wordsTextarea.value;
+	let wordsTrimmed = words.replace(/\s+/g, ' ').trim();
+	let splitWords = wordsTrimmed.split(' ');
+
+	// console.log(splitWords);
+
+	let numberOfWords = splitWords.length;
+	if (splitWords[0] == '') {
+		numberOfWords = 0;
+	}
+	wordCount.innerHTML = numberOfWords;
+
+	// calculate the cost of this translation
+	let costInEuros = 0;
+
+	const calculateCost = () => {
+		// calculation
+		let calculatedCostInEuros = numberOfWords * pricePerWordInEuros;
+		costInEuros += calculatedCostInEuros;
+	};
+
+	calculateCost();
+
+	if (costInEuros === 0) {
+		displayPrice.innerText = ' ';
+	} else {
+		displayPrice.innerText = 'Cost: ' + costInEuros.toFixed(2) + ' €.';
+	}
+};
+
+countBtn.addEventListener('click', countWords);
