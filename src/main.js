6130fcc648d48bbf2db180e85bc268a0aa532dd2
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-10-14 12:24:57 +0100

    markup update

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const wordsTextarea = document.querySelector(
+	'.custom-container .inputed-words'
+);
+const countBtn = document.querySelector('.custom-container .count-btn');
+const wordCount = document.querySelector(
+	'.custom-container .words-detected span'
+);
+const displayPrice = document.getElementById('display-price');
