747f707ec6807b7c09e89432cf77ed3b139fd85b
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-10-14 12:35:19 +0100

    add reset btn logic

--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@ const wordCount = document.querySelector(
 	'.custom-container .words-detected span'
 );
 const displayPrice = document.getElementById('display-price');
+
+
+// RESET BTN
+const resetPage = () =>
+{
+	location.reload();
+}
+
+const resetBtn = document.getElementById('reset-btn').addEventListener("click", resetPage)
