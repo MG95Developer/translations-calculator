78f586160293c00e8bd08bb67f367157fddbd1b7
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-10-14 12:13:27 +0100

    add markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				'custom-black': '#30363d',
+				'custom-white': '#f6f8fa',
+				'custom-green': 'rgb(126, 231, 135)',
+				'call-to-action': '#dd7df7',
+			},
+		},
+	},
+	plugins: [],
+};
 
